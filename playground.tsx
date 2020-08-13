76e63e6963bcb771ef185daab943dd582c521ae7
makibishi <makibishi0212@gmail.com>
2020-08-14 03:07:23 +0900

    step 3

--- playground.tsx
+++ playground.tsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { RecoilRoot, useRecoilValue, useRecoilState, atom } from "recoil";
+
+const TAB = {
+  HTML: 0,
+  CSS: 1,
+  JS: 2,
+} as const;
+
+const activeTab = atom<number>({ key: "active", default: TAB.HTML });
 
 export const PlayGroundEditor: React.FC<{}> = () => {
   return (
-    <div className="playground-render">
-      <PlayGroundResult></PlayGroundResult>
-      <div className="playground-edit">
-        <PlayGroundNav></PlayGroundNav>
-        <SourceEditor></SourceEditor>
+    <RecoilRoot>
+      <div className="playground-render">
+        <PlayGroundResult></PlayGroundResult>
+        <div className="playground-edit">
+          <PlayGroundNav></PlayGroundNav>
+          <SourceEditor></SourceEditor>
+        </div>
       </div>
-    </div>
+    </RecoilRoot>
   );
 };
 
@@ -19,11 +30,38 @@ const PlayGroundResult: React.FC<{}> = () => {
 
 // html,css,jsの切替えタブ
 const PlayGroundNav: React.FC<{}> = () => {
+  const [active, changeActive] = useRecoilState(activeTab);
+
+  const activeClass = (tab: number) => {
+    return active === tab ? "is-active" : "";
+  };
+
   return (
     <div className="playground-nav">
-      <a className={`playground-nav__link is-active`}>HTML</a>
-      <a className={`playground-nav__link`}>CSS</a>
-      <a className={`playground-nav__link`}>JS</a>
+      <a
+        className={`playground-nav__link ${activeClass(TAB.HTML)}`}
+        onClick={() => {
+          changeActive(TAB.HTML);
+        }}
+      >
+        HTML
+      </a>
+      <a
+        className={`playground-nav__link ${activeClass(TAB.CSS)}`}
+        onClick={() => {
+          changeActive(TAB.CSS);
+        }}
+      >
+        CSS
+      </a>
+      <a
+        className={`playground-nav__link ${activeClass(TAB.JS)}`}
+        onClick={() => {
+          changeActive(TAB.JS);
+        }}
+      >
+        JS
+      </a>
     </div>
   );
 };
